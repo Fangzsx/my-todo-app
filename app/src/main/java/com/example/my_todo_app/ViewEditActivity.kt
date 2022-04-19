77251024bb00174c19321ebd69cfec315572673c
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-04-19 19:45:23 +0800

    do not save feature if the current text = original text

--- app/src/main/java/com/example/my_todo_app/ViewEditActivity.kt
+++ app/src/main/java/com/example/my_todo_app/ViewEditActivity.kt
@@ -3,6 +3,9 @@ package com.example.my_todo_app
 import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import androidx.core.content.ContextCompat
 import com.example.my_todo_app.databinding.ActivityViewEditBinding
 
@@ -20,6 +23,27 @@ class ViewEditActivity : AppCompatActivity() {
                     ContextCompat.getColor(this@ViewEditActivity, R.color.gray))
             }
         }
+
+        val currentText = binding.etNote.text.toString()
+
+        binding.etNote.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                binding.btnSave.visibility = View.VISIBLE
+
+                if(currentText == binding.etNote.text.toString()){
+                    binding.btnSave.visibility = View.GONE
+                }
+            }
+
+        })
 
     }
 }
