117a4fad4c7e83477361d073c9fdfcb0202f1e17
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-04-19 09:28:40 +0800

    change color of add note edit text's hint, and hint text upon focused

--- app/src/main/java/com/example/my_todo_app/AddNoteActivity.kt
+++ app/src/main/java/com/example/my_todo_app/AddNoteActivity.kt
@@ -1,11 +1,33 @@
 package com.example.my_todo_app
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.my_todo_app.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityAddNoteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_note)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.etNote.setOnFocusChangeListener(object : View.OnFocusChangeListener{
+            override fun onFocusChange(p0: View?, hasFocus: Boolean) {
+                if(hasFocus){
+                    binding.textInputLayout.hint = "I am planning to ..."
+                    binding.textInputLayout.defaultHintTextColor = ColorStateList.valueOf(
+                        ContextCompat.getColor(this@AddNoteActivity, R.color.gray))
+                }
+            }
+
+        })
     }
 }
